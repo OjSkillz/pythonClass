5711c55f202ac74d552bf3d1c3058c93ab739c09
OjSkillz <soojo06@gmail.com>
2024-10-23 07:44:59 +0100

    assignments updated

---
+++ assignments/tableOfPowers/tableOfPowers.py
@@ -0,0 +1,17 @@
+# Table of Powers Display
+
+counter = 1
+a = 0
+b = 1
+
+print("\na\t b\t pow(a, b)");
+
+while counter <= 5 :
+		
+   a += 1 
+   b += 1
+	 
+   print(a, "\t", b, "\t",(a ** b))
+	
+   counter += 1
+	  
