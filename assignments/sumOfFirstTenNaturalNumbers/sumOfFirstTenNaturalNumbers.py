a231751c481939da8d9268900ae730fa9fbf0f35
OjSkillz <soojo06@gmail.com>
2024-10-23 07:50:16 +0100

    assignments repo updated

---
+++ assignments/sumOfFirstTenNaturalNumbers/sumOfFirstTenNaturalNumbers.py
@@ -0,0 +1,14 @@
+# Sum of First 10 natural numbers
+
+naturalNumber = 1
+sum = 0
+		    
+while naturalNumber <=10 :
+
+   sum = sum + naturalNumber
+
+   naturalNumber += 1 
+
+
+print("\nThe sum of the first ten natural numbers is", sum)
+	    
