cdb6ac7262384be9bf80093f1e04ef0a3a1489b3
OjSkillz <soojo06@gmail.com>
2024-10-30 09:08:22 +0100

    Vowels Or Consonants?

---
+++ assignments/assignment4/vowelOrConsonantDisplay.py
@@ -0,0 +1,26 @@
+
+    
+userLetter = input("\nInput an alphabet: ")
+    
+      
+min = "a";
+max = "z";
+
+if (len(userLetter) == 1 and userLetter >= min and userLetter <= max) :
+    
+    if (userLetter == "a") :
+        print(f"\nInput letter is a vowel")
+
+    elif (userLetter == "e") :
+        print(f"\nInput letter is a vowel")
+    elif (userLetter == "i") :
+        print(f"\nInput letter is a vowel")
+      
+    elif (userLetter == "o") :
+        print(f"\nInput letter is a vowel")
+          
+    elif (userLetter == "u") :
+        print(f"\nInput letter is a vowel")
+              
+    else :
+        print(f"\nInput letter is a consonant")
