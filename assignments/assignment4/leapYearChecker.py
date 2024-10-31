ef459dc41aa883c662e5391d6917e045e4724c5a
OjSkillz <soojo06@gmail.com>
2024-10-31 02:39:39 +0100

    Leap Year Checker

---
+++ assignments/assignment4/leapYearChecker.py
@@ -0,0 +1,8 @@
+year = int(input("\nEnter any year to check whether it is a leap year: "))
+
+    
+if year % 4 == 0  :
+   print(f"\n{year} is a leap year")
+    
+else :
+    print(f"\n{year} is not a leap year")
