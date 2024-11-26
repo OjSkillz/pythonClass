b34621012e4bc425337f2a2a9748b456399898b9
OjSkillz <soojo06@gmail.com>
2024-11-26 06:35:25 +0100

    Weekend Snack Function Updated

---
+++ weekendsnackfunctions/functionsubtract.py
@@ -0,0 +1,2 @@
+def subtract(integer1, integer2)->int:
+    return abs(integer1 - integer2)
---
+++ weekendsnackfunctions/test_functionsubtract.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+import functionsubtract
+
+class TestFunctionSubtract(TestCase):
+    def test_that_function_subtract_exists(self):
+        functionsubtract.subtract(5, 7)
+    
+    def test_that_function_subtract_returns_correct_value(self):
+        actual = functionsubtract.subtract(5, 7)
+        expected = 2
+        self.assertEqual(actual, expected)
