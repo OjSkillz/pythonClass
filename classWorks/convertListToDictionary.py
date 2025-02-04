a2677498cf2629d07dd92de1f24ae17b38853361
Samuel Ojo <soojo06@gmail.com>
2025-02-04 19:41:44 +0000

    Class works updated

--- classWorks/convertListToDictionary.py
+++ classWorks/convertListToDictionary.py
@@ -1,5 +1,5 @@
 def convertListToDictionary(numbers: list) :
-    return {element:number.count(element) for element in sorted(numbers)}
+    return {element:numbers.count(element) for element in sorted(numbers)}
 
 elements = [2,2,1,3,5,5]
 print(convertListToDictionary(elements))
