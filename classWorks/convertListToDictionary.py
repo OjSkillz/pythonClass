2508fb7896dca34bdc0e08bef0c11ac8adfaba11
OjSkillz <soojo06@gmail.com>
2025-02-04 10:04:46 +0100

    Function to convert a list to a dictionary of its elements as element:occurrence pairs updated

--- classWorks/convertListToDictionary.py
+++ classWorks/convertListToDictionary.py
@@ -1,8 +1,5 @@
 def convertListToDictionary(numbers: list) :
-    my_dict = {}
-    for element in sorted(numbers) :
-        my_dict[element] = numbers.count(element)
-    return my_dict
+    return {element:numbers.count(element) for element in sorted(numbers)}
 
 elements = [2,2,1,3,5,5]
 print(convertListToDictionary(elements))
