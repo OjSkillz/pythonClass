c9c589a7d6aac1f4b28c2cd0fd274866c48280ee
OjSkillz <soojo06@gmail.com>
2025-02-27 10:48:14 +0100

    "Getting Character Frequency"

---
+++ tests/test_functioncharacterfrequency.py
@@ -0,0 +1,19 @@
+import unittest
+import classWorks.functioncharacterfrequency as cf
+
+class TestFunctionCharacterFrequency(unittest.TestCase):
+    def test_that_function_exists(self):
+        cf.get_character_frequency("semicolon.africa")
+
+    def test_that_function_returns_correct_value(self):
+        sample_string = "semicolon.africa"
+        expected = {'s':1,'e':1,'m':1,'i':2,'c':2,'o':2,'l':1,'n':1,'.':1,'a':2,'f':1,'r':1}
+
+        actual = cf.get_character_frequency(sample_string)
+
+        self.assertEqual(expected, actual)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
