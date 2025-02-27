eb586f53826b26b595661be223cd4a5ee602d79b
OjSkillz <soojo06@gmail.com>
2025-02-27 10:51:00 +0100

    "Getting Character Frequency"

--- classWorks/functioncharacterfrequency.py
+++ classWorks/functioncharacterfrequency.py
@@ -1,5 +1,5 @@
 def get_character_frequency(string):
-    character_frequency = {}
+    character_frequencies = {}
     for character in string:
-        character_frequency[character] = string.count(character)
-    return character_frequency
+        character_frequencies[character] = string.count(character)
+    return character_frequencies
--- tests/test_functioncharacterfrequency.py
+++ tests/test_functioncharacterfrequency.py
@@ -7,11 +7,11 @@ class TestFunctionCharacterFrequency(unittest.TestCase):
 
     def test_that_function_returns_correct_value(self):
         sample_string = "semicolon.africa"
-        expected = {'s':1,'e':1,'m':1,'i':2,'c':2,'o':2,'l':1,'n':1,'.':1,'a':2,'f':1,'r':1}
+        character_frequencies = {'s':1,'e':1,'m':1,'i':2,'c':2,'o':2,'l':1,'n':1,'.':1,'a':2,'f':1,'r':1}
 
         actual = cf.get_character_frequency(sample_string)
 
-        self.assertEqual(expected, actual)
+        self.assertEqual(character_frequencies, actual)
 
 
 
